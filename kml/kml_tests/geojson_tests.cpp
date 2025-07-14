e84efc1bb66a742334074cdb78a38510218eaa41
S. Kozyr <s.trump@gmail.com>
2025-07-14 11:59:34 +0300

    Tests POC
    Signed-off-by: S. Kozyr <s.trump@gmail.com>

--- kml/kml_tests/geojson_tests.cpp
+++ kml/kml_tests/geojson_tests.cpp
@@ -8,10 +8,15 @@
 #include "coding/file_reader.hpp"
 
 #include "platform/platform.hpp"
+#include "coding/serdes_json.hpp"
+
+#include "base/exception.hpp"
 
 namespace geojson_tests
 {
-static kml::FileData LoadGpxFromString(std::string_view content)
+
+/*
+static kml::FileData LoadGeojsonFromString(std::string_view content)
 {
   TEST_NO_THROW(
   {
@@ -62,17 +67,82 @@ UNIT_TEST(GeoJson_Parse_Basic)
   ]
 })";
 
-  kml::FileData data;
-  kml::BookmarkData bookmarkData;
-  bookmarkData.m_name[kml::kDefaultLang] = "Waypoint 1";
-  bookmarkData.m_point = mercator::FromLatLon(42.81025, -1.65727);
-  bookmarkData.m_customName[kml::kDefaultLang] = "Waypoint 1";
-  bookmarkData.m_color = {kml::PredefinedColor::Red, 0};
-  data.m_bookmarksData.emplace_back(std::move(bookmarkData));
+  kml::FileData const dataFromText = LoadGeojsonFromString(input);
 
-  kml::FileData const dataFromText = LoadGpxFromString(input);
+  //TEST_EQUAL(dataFromText, data, ());
+}
+*/
 
-  TEST_EQUAL(dataFromText, data, ());
+struct GeoJsonGeometry2 {
+    std::vector<geometry::PointWithAltitude> m_coordinates;
+
+    template <typename Visitor>
+    void Visit(Visitor & visitor)
+    {
+        try {
+            std::vector<double> coordData;
+            visitor(coordData, "coordinates");
+            if(coordData.size() != 2) {
+                //ERROR!
+            }
+        }
+        catch(RootException exc) {
+            std::vector<std::pair<double, double>> polygonData;
+            visitor(polygonData, "coordinates");
+        }
+    }
+
+    template <typename Visitor>
+    void Visit(Visitor & visitor) const
+    {
+        std::vector<double> coordData;
+        visitor(coordData, "coordinates");
+        // TODO
+    }
+
+    friend std::string DebugPrint(GeoJsonGeometry2 const & c)
+    {
+        DebugPrintVisitor visitor("GeoJsonGeometry");
+        c.Visit(visitor);
+        return visitor.ToString();
+    }
+};
+
+
+UNIT_TEST(Geometry_parser)
+{
+
+    std::string_view constexpr input_case_way = R"(
+    {
+      "coordinates": [
+        [
+          14.949382505528291,
+          8.16007148457335
+        ],
+        [
+          26.888888114204264,
+          9.708105796659268
+        ],
+        [
+          37.54707497642465,
+          6.884595662842159
+        ]
+      ]
+    })";
+
+    std::string_view constexpr input_case_point = R"(
+    {
+      "coordinates": [
+        31.02177966625902,
+        29.8310316130992
+      ]
+    })";
+
+    GeoJsonGeometry2 geometry;
+    MemReader input(input_case_way);
+    NonOwningReaderSource source(input);
+    coding::DeserializerJson des(source);
+    des(geometry);
 }
 
 }  // namespace gpx_tests
